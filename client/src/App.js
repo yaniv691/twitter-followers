fc5570f3f2955dc5d99b464059ab78bcf8ca4423
Yaniv Wainer <yaniv.wainer@oracle.com>
2019-04-20 10:54:09 +0300

    React app client

---
+++ client/src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state = {
+    followers: '',
+    post: '',
+    responseToPost: '',
+  };
+  componentDidMount() {
+    this.callApi()
+      .then(res => {
+        console.log(res);
+        this.setState({ followers: res.users })
+      })
+      .catch(err => console.log(err));
+  }
+  callApi = async () => {
+    const response = await fetch('/api/followers');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  };
+  render() {
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
