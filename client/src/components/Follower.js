227dc7bd3b8489393fca58b7a0b493ada996df66
Yaniv Wainer <yaniv.wainer@oracle.com>
2019-04-23 11:10:39 +0300

    Table styling

--- client/src/components/Follower.js
+++ client/src/components/Follower.js
@@ -2,7 +2,13 @@ import React from 'react';
 export default function Follower(props) {
   return (
     <tr>
-      <td><img src={props.followerDetails.profile_image_url} alt={`${props.followerDetails.name} Twitter Profile Avatar`} /></td>
+      <td>
+        <img
+          className="profile-image"
+          src={props.followerDetails.profile_image_url}
+          alt={`${props.followerDetails.name} Twitter Profile`}
+        />
+      </td>
       <td>{props.followerDetails.name}</td>
       <td><a href={`https://twitter.com/${props.followerDetails.screen_name}`} target="_blank" rel="noopener noreferrer">
         @{props.followerDetails.screen_name}
