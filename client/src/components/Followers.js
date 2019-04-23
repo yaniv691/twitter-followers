0b420ba281a5b34d15b12b08f868de0d8f1a924a
Yaniv Wainer <yaniv.wainer@oracle.com>
2019-04-23 11:10:25 +0300

    Sort buttons above table

--- client/src/components/Followers.js
+++ client/src/components/Followers.js
@@ -1,29 +1,61 @@
 import React from 'react';
 import Follower from './Follower';
 import Table from 'react-bootstrap/Table';
-function Followers(props) {
-  if (!props.users) {
-    return (<div className="text-center"><span className="emoji" role="img" aria-label="facepalm">🤷🏻‍♂️</span>Oops, no such user.</div>);
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+
+class Followers extends React.Component {
+  sortByName = () => {
+    this.props.sortFunction('name');
   }
-  if (props.users.length === 0) {
-    return (<div className="text-center"><span className="emoji" role="img" aria-label="disappointed">😞</span>{props.screenNameInput} doesn't have any followers yet.</div>);
+
+  sortByScreenName = () => {
+    this.props.sortFunction('screen_name');
   }
-  return (
-    <Table striped bordered hover responsive>
-      <thead>
-        <tr>
-          <th></th>
-          <th onClick={() => props.sortFunction('name')} className="sortable">Account Name</th>
-          <th onClick={() => props.sortFunction('screen_name')} className="sortable">Screen Name</th>
-          <th>Following</th>
-          <th>Followers</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.users.map(user => <Follower key={user.id} followerDetails={user} />)}
-      </tbody>
-    </Table>
-  );
+
+  render() {
+    if (!this.props.users) {
+      return (
+        <div className="text-center">
+          <span className="emoji" role="img" aria-label="facepalm">🤷🏻‍♂️</span>
+          Oops, no such user.
+        </div>
+      );
+    }
+
+    if (this.props.users.length === 0) {
+      return (
+        <div className="text-center">
+          <span className="emoji" role="img" aria-label="disappointed">😞</span>
+          {this.props.screenNameInput} doesn't have any followers yet.
+        </div>
+      );
+    }
+
+    return (
+      <div class="followers-list-container">
+        <small>Sort by:</small>
+        <ButtonGroup size="sm" >
+          <Button variant="link" onClick={this.sortByName}>Account Name</Button>
+          <Button variant="link" onClick={this.sortByScreenName}>Screen Name</Button>
+        </ButtonGroup>
+        <Table striped hover responsive className="followers-list">
+          <thead>
+            <tr>
+              <th></th>
+              <th onClick={this.sortByName} className="sortable">Account Name</th>
+              <th onClick={this.sortByScreenName} className="sortable">Screen Name</th>
+              <th>Following</th>
+              <th>Followers</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.users.map(user => <Follower key={user.id} followerDetails={user} />)}
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
 }
 
 export default Followers;
