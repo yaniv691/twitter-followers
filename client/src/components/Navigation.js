5f5f94e935c80ac4539c51c34e005b494390d14e
Yaniv Wainer <yaniv.wainer@oracle.com>
2019-04-23 10:35:14 +0300

    Style navigation as link buttons

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -3,8 +3,8 @@ import Button from 'react-bootstrap/Button';
 function Navigation(props) {
   return (
     <div className="navigation">
-      <Button disabled={props.prevCursor === "0"} onClick={() => props.navigateFollowers('prevCursor')}>&larr; Previous</Button>
-      <Button disabled={props.nextCursor === "0"} onClick={() => props.navigateFollowers('nextCursor')} className="float-right">Next &rarr;</Button>
+      <Button variant="link" disabled={props.prevCursor === "0"} onClick={() => props.navigateFollowers('prevCursor')}>&larr; Previous</Button>
+      <Button variant="link" disabled={props.nextCursor === "0"} onClick={() => props.navigateFollowers('nextCursor')} className="float-right">Next &rarr;</Button>
     </div>
   );
 }
